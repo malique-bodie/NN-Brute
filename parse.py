600360491901a5acb483fd1afcd52d2a84b277e0
malique-bodie <71160334+malique-bodie@users.noreply.github.com>
2022-06-15 08:44:50 -0700

    Add files via upload

--- parse.py
+++ parse.py
@@ -8,8 +8,8 @@ def format_data(fp):
     index_df = df.iloc[:10001,0:]
     query_df = df.iloc[10001:10101,0:]
     #writing to txt file
-    index_df.to_csv('index_vec.txt', sep=' ', header=None, index=None)
-    query_df.to_csv('query_vec.txt', sep=' ', header=None, index=None)
+    index_df.to_csv('index_vec.txt', sep='|', header=None, index=None)
+    query_df.to_csv('query_vec.txt', sep='|', header=None, index=None)
 
 format_data('./glove.6B.50d.txt')
 
